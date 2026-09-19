d88d550060f85f9b07f6d29ff1a712116a427de5
Akshat Mittal <Akshat.38005@stu.upes.ac.in>
2026-09-20 00:34:44 +0530

    Day01

---
+++ Day01/q2.c
@@ -0,0 +1,34 @@
+/*
+*Name : Akshat Mittal
+*Sap ID : 590038005
+*Day : 1
+*Date : 10/08/26
+*
+*PROBLEM STATEMENT:
+*Write a program to input two numbers and display their sum, difference, product, and quotient.
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	double firstNumber, secondNumber;
+
+	printf("Enter two numbers: ");
+	scanf("%lf %lf", &firstNumber, &secondNumber);
+
+	printf("Sum = %.2lf\n", firstNumber + secondNumber);
+	printf("Difference = %.2lf\n", firstNumber - secondNumber);
+	printf("Product = %.2lf\n", firstNumber * secondNumber);
+
+	if (secondNumber != 0)
+	{
+		printf("Quotient = %.2lf\n", firstNumber / secondNumber);
+	}
+	else
+	{
+		printf("Quotient is undefined because the second number is zero.\n");
+	}
+
+	return 0;
+}
